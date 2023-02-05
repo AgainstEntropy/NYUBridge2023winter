235d377415def520a440a128c6038326d775c867
Yihao Wang <yihaowang18@fudan.edu.cn>
2023-02-05 14:14:53 +0800

    remove functions in hw4_q6

--- hw4/yw7486_hw4_q6.cpp
+++ hw4/yw7486_hw4_q6.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 using namespace std;
 
-bool qualified(int num);
-
 int main()
 {
     int n;
     cout << "Enter a positive integer: ";
     cin >> n;
 
+    int digit, even, odd, num;
     for (int i = 1; i <= n; i++)
     {
-        if (qualified(i))
+        
+        even = odd = 0;
+        num = i;
+        while (num != 0)
+        {
+            digit = num % 10;
+            if (digit % 2)
+            {
+                odd++;
+            }
+            else
+            {
+                even++;
+            }
+            num /= 10;
+        }
+        if (even > odd)
         {
             cout << i << endl;
         }
@@ -19,27 +34,3 @@ int main()
 
     return 0;
 }
-
-bool qualified(int num)
-{
-    int digit;
-    int even = 0,
-        odd = 0;
-    while (num != 0)
-    {
-        digit = num % 10;
-        if (digit % 2)
-        {
-            odd++;
-        }
-        else
-        {
-            even++;
-        }
-        num /= 10;
-    }
-
-    return true ? even > odd : false;
-}
-
-// more even digits than odd digits
