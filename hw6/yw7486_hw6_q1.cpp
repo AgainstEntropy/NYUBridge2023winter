02da5069f39bd4b73f52f573e6c224d87d11a7b3
Yihao Wang <yihaowang18@fudan.edu.cn>
2023-02-14 21:25:53 +0800

    add hw6

---
+++ hw6/yw7486_hw6_q1.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+using namespace std;
+
+int fib(int n);
+
+int main()
+{
+    int num;
+    cout << "Please enter a positive integer: ";
+    cin >> num;
+
+    cout << fib(num);
+
+    return 0;
+}
+
+int fib(int n)
+{
+    if (n <= 2)
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
