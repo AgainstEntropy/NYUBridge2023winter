ffe48cf9a5291f6a21ca1c7494e36c3e8dceec5e
Yihao Wang <yihaowang18@fudan.edu.cn>
2023-03-15 00:07:04 +0800

    add hw10_q1

---
+++ hw10/yw7486_hw10_q1.cpp
@@ -0,0 +1,47 @@
+# include <iostream>
+using namespace std;
+
+string* createWordsArray(string sentence, int& outWordsArrSize);
+int countWords(string sentence);
+
+const char SPACE = ' ';
+
+int main() {
+    string sentence("You can do it");
+    
+    int out_words_arr_size;
+    string* words_array = createWordsArray(sentence, out_words_arr_size);
+
+    for (int i = 0; i < out_words_arr_size; i++) {
+        cout << words_array[i] << endl;
+    }
+    delete [] words_array;
+    
+    return 0;
+}
+
+string* createWordsArray(string sentence, int& outWordsArrSize) {
+    outWordsArrSize = countWords(sentence);
+    int word_len;
+    string word(""), *words_array = new string[outWordsArrSize];
+    for (int i = 0; i < outWordsArrSize; i++) {
+        word_len = sentence.find(SPACE);
+        words_array[i] = sentence.substr(0, word_len);
+        sentence = sentence.substr(word_len + 1, sentence.length());
+    }
+
+    return words_array;
+}
+
+int countWords(string sentence) {
+    int word_len, num_of_words = 0;
+    string word("");
+    while (word != sentence) {
+        word_len = sentence.find(SPACE);
+        word = sentence.substr(0, word_len);
+        sentence = sentence.substr(word_len + 1, sentence.length());
+        num_of_words++;
+    }
+
+    return num_of_words;
+}
