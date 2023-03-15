bbfa851217b24e57216fde3e0ea8f82a8925dfeb
Yihao Wang <yihaowang18@fudan.edu.cn>
2023-03-16 01:13:26 +0800

    add hw10_q2

---
+++ hw10/yw7486_hw10_q2.cpp
@@ -0,0 +1,65 @@
+# include <iostream>
+using namespace std;
+
+int* findMissing(int arr[], int n, int& resArrSize);
+int sumArray(int* arr, int arrSize);
+void printArray(int* arr, int arrSize);
+
+const char SPACE = ' ';
+
+int main() {
+    int arr[] = {3, 1, 3, 0, 6, 4};
+
+    int res_arr_size;
+    int* missing_arr = findMissing(arr, 6, res_arr_size);
+
+    cout << "There are " << res_arr_size << " numbers missing, which are(is): ";
+    printArray(missing_arr, res_arr_size);
+
+    delete [] missing_arr;
+
+    return 0;
+}
+
+int* findMissing(int arr[], int n, int& resArrSize) {
+    int* flag_arr = new int[n+1];
+    for (int i = 0; i < n+1; i++) {
+        flag_arr[i] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        flag_arr[arr[i]] = 0;
+    }
+    resArrSize = sumArray(flag_arr, n+1);
+
+    int* miss_arr = new int[resArrSize];
+    int miss_index = 0;
+    for (int i = 0; i < n+1; i++) {
+        if (flag_arr[i]) {
+            miss_arr[miss_index] = i;
+            miss_index++;
+        }
+        if (miss_index == resArrSize) {
+            delete [] flag_arr;
+            break;
+        }
+    }
+
+    return miss_arr;
+}
+
+int sumArray(int* arr, int arrSize) {
+    int sum = 0;
+    for (int i = 0; i < arrSize; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+void printArray(int* arr, int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        cout << arr[i] << SPACE;
+    }
+    cout << endl;
+}
