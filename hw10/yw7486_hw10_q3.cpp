0f066d6654d934dc40f13b394f7a40f624504f55
Yihao Wang <yihaowang18@fudan.edu.cn>
2023-03-17 10:26:54 +0800

    add hw10_q3 main2

--- hw10/yw7486_hw10_q3.cpp
+++ hw10/yw7486_hw10_q3.cpp
@@ -1,11 +1,13 @@
 # include <iostream>
+# include <vector>
 using namespace std;
 
 void main1();
 void main2();
-void printArray(int* arr, int arrSize, string sep, string end);
 int* initArray(int arrSize);
 int* appendArray(int arr[], int& arrSize, int newNum);
+void printArray(int* arr, int arrSize, string sep, string end);
+void printVector(vector<int> vec, string sep, string end);
 
 const string SPACE = " ";
 const string PROMPT_1 = "Please enter a sequence of positive integers, each in a separate line.\n"
@@ -14,9 +16,11 @@ const string PROMPT_2 = "Please enter a number you want to search.\n";
 const string NEGATIVE_MESSAGE = " does not show at all in the sequence.";
 
 int main() {
+    cout<< "section a" <<endl;
     main1();
 
-    // main2();
+    cout<< "section b" <<endl;
+    main2();
 
     return 0;
 }
@@ -44,21 +48,42 @@ void main1() {
             indexs = appendArray(indexs, frequency, i);
         }
     }
-    cout << to_string(search_num) + " shows in lines ";
-    printArray(indexs, frequency, ", ", ".");
+
+    if (frequency == 0) {
+        cout << to_string(search_num) + NEGATIVE_MESSAGE;
+    } else {
+        cout << to_string(search_num) + " shows in lines ";
+        printArray(indexs, frequency, ", ", ".\n");
+    }
 }
 
 void main2() {
     cout << PROMPT_1;
-}
 
-void printArray(int* arr, int arrSize, string sep = ", ", string end = ".") {
-    string printed = "";
-    for (int i = 0; i < arrSize-1; i++) {
-        printed += (to_string(arr[i]) + sep);
+    vector<int> arr;
+    int new_num;
+    while (true) {
+        cin >> new_num;
+        if (new_num == -1) break;
+        arr.push_back(new_num);
     }
-    printed += (to_string(arr[arrSize-1]) + end);
-    cout << printed << endl;
+
+    cout << PROMPT_2;
+    int search_num;
+    cin >> search_num;
+
+    vector<int> indexs;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == search_num) {
+            indexs.push_back(i+1);
+        }
+    }
+    if (indexs.empty()) {
+        cout << to_string(search_num) + NEGATIVE_MESSAGE;
+    } else {
+        cout << to_string(search_num) + " shows in lines ";
+        printVector(indexs, ", ", ".\n");
+    }
 }
 
 int* initArray(int arrSize) {
@@ -78,4 +103,22 @@ int* appendArray(int arr[], int& arrSize, int newNum) {
     new_arr[arrSize] = newNum;
     arrSize++;
     return new_arr;
+}
+
+void printArray(int* arr, int arrSize, string sep = ", ", string end = ".") {
+    string printed = "";
+    for (int i = 0; i < arrSize-1; i++) {
+        printed += (to_string(arr[i]) + sep);
+    }
+    printed += (to_string(arr[arrSize-1]) + end);
+    cout << printed << endl;
+}
+
+void printVector(vector<int> vec, string sep = ", ", string end = ".") {
+    string printed = "";
+    for (int i = 0; i < vec.size()-1; i++) {
+        printed += (to_string(vec[i]) + sep);
+    }
+    printed += (to_string(vec[vec.size()-1]) + end);
+    cout << printed << endl;
 }
