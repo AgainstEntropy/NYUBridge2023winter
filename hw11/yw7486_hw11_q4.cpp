4d378b28888fd5ddeb3b95fa9b85f3255367718d
Yihao Wang <yihaowang18@fudan.edu.cn>
2023-03-22 11:19:39 +0800

    add hw11 q3,q4

---
+++ hw11/yw7486_hw11_q4.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+# include <cmath>
+using namespace std;
+
+int minCost(int arr[], int arrSize);
+
+int main() {
+    int arr[6] = {0, 3, 80, 6, 57, 10};
+    int arr_size = 6;
+
+    cout << "The lowest cost is " << minCost(arr, arr_size);
+
+    return 0;
+}
+
+int minCost(int arr[], int arrSize) {
+    if (arrSize <= 2) {
+        return arr[arrSize - 1];
+    } else {
+        int min1 = minCost(arr, arrSize - 1) + arr[arrSize - 1];
+        int min2 = minCost(arr, arrSize - 2) + arr[arrSize - 1];
+        return (min1 < min2) ? min1 : min2;
+    }
+}
