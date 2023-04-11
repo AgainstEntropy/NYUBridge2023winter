596e055b54db8f0d43b8b30bb14aaa7d8d1437f9
Yihao Wang <yihaowang18@fudan.edu.cn>
2023-04-11 20:51:20 +0800

    complete hw13

--- hw13/yw7486_hw13.cpp
+++ hw13/yw7486_hw13.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-const int WORLD_HEIGHT = 5, 
-          WORLD_WIDTH = 5;
+const int WORLD_HEIGHT = 20, 
+          WORLD_WIDTH = 20;
 const int ANT_BREED_INTERVAL = 3,
           BUG_BREED_INTERVAL = 8,
           BUG_STARVE = 3;
@@ -26,53 +26,53 @@ class Organism
 {
 public:
     Organism()
-    : type(EMPTY), breed_interval(-1), position(-1, -1), moved(false) {}
-    // Organism(Position pos)
-    // : type(EMPTY), breed_interval(-1), position(pos) {}
-    Organism(char type, int breed_interval, int h, int w) 
-    : type(type), breed_interval(breed_interval), position(h, w), moved(false) {}
+    : type(EMPTY), breed_interval(-1), steps_since_breed(-1), position(-1, -1), moved(false) {}
+    Organism(char type, int h, int w) 
+    : type(type), steps_since_breed(0), position(h, w), moved(false) {
+        breed_interval = (type == ANT) ? ANT_BREED_INTERVAL : BUG_BREED_INTERVAL;
+    }
 
     friend ostream &operator<<(ostream &outs, const Organism &o) { 
         outs << o.type; return outs; }
 
-    // int get_bread_interval() const { return breed_interval; }
     char get_type() { return type; }
     Position get_position() { return position; }
     bool has_moved() { return moved; }
+    int get_breed_steps() const { return steps_since_breed; }
 
     void set_position(int h, int w) { position.h = h; position.w = w; }
     void set_position(Position new_pos) { position = new_pos; }
     void set_moved(bool status) { moved = status; }
+    void reset_breed_steps() { steps_since_breed = 0; }
 
-    vector<Position> get_target_positions(Organism*** world, int height, int width, char target) const;
+    vector<Position> get_target_positions(Organism*** world, char target) const;
     Position get_random_position(Organism*** world);
 
-    void print_position() const {
-        cout << '(' << position.h << ',' << position.w << ")\n"; }
-
-    virtual void move(Organism*** world) {};
+    virtual void move(Organism*** world) {}
+    Position breed(Organism*** world);
 
 private:
     char type;
     Position position;
     bool moved;
     int breed_interval;
+    int steps_since_breed;
 };
 
-vector<Position> Organism::get_target_positions(Organism*** world, int height, int width, char target) const {
+vector<Position> Organism::get_target_positions(Organism*** world, char target) const {
     int candidates[] = { -1, 1 }, h, w;
     vector<Position> positions;
     for (int dh : candidates) {
         h = position.h + dh;
         w = position.w;
-        if ((0<=h && h<height) && (world[h][w]->get_type() == target)) {
+        if ((0<=h && h<WORLD_HEIGHT) && (world[h][w]->get_type() == target)) {
             positions.push_back(Position(h, w));
         }
     }
     for (int dw : candidates) {
         h = position.h;
         w = position.w + dw;
-        if ((0<=w && w<width) && (world[h][w]->get_type() == target)) {
+        if ((0<=w && w<WORLD_WIDTH) && (world[h][w]->get_type() == target)) {
             positions.push_back(Position(h, w));
         }
     }
@@ -115,19 +115,34 @@ Position Organism::get_random_position(Organism*** world) {
     return new_pos;
 }
 
+Position Organism::breed(Organism*** world) {
+    Position breed_pos;
+    if (++steps_since_breed == breed_interval) {
+        reset_breed_steps();
+        vector<Position> empty_positions = get_target_positions(world, EMPTY);
+        int empty_num = empty_positions.size();
+        if (empty_num != 0) {
+            breed_pos = empty_positions[rand() % empty_num];
+            delete world[breed_pos.h][breed_pos.w];
+        }
+    }
+    return breed_pos;
+}
+
 
 class Ant : public Organism
 {
 public:
-    Ant(int x, int y) : Organism(ANT, ANT_BREED_INTERVAL, x, y) {}
+    Ant(int x, int y) : Organism(ANT, x, y) {}
+    Ant(Position pos) : Organism(ANT, pos.h, pos.w) {}
 
     virtual void move(Organism*** world);
 };
 
 void Ant::move(Organism*** world) {
+    // moving
     Position ori_pos = get_position();
     Position new_pos = get_random_position(world);
-    
     if (new_pos.h != -1) {
         set_position(new_pos);
         delete world[new_pos.h][new_pos.w];
@@ -135,8 +150,12 @@ void Ant::move(Organism*** world) {
         world[ori_pos.h][ori_pos.w] = new Organism;
     }
     set_moved(true);
-    // breed
-    // vector<Position> positions;
+
+    // breeding
+    Position breed_pos = breed(world);
+    if (breed_pos.h != -1) {
+        world[breed_pos.h][breed_pos.w] = new Ant(breed_pos);
+    }
 }
 
 
@@ -144,25 +163,29 @@ class Doodlebug : public Organism
 {
 public:
     Doodlebug(int x, int y) 
-    : Organism(DOODLEBUG, BUG_BREED_INTERVAL, x, y), starve_steps(BUG_STARVE) {}
+    : Organism(DOODLEBUG, x, y), steps_until_starve(BUG_STARVE) {}
+    Doodlebug(Position pos) 
+    : Organism(DOODLEBUG, pos.h, pos.w), steps_until_starve(BUG_STARVE) {}
 
-    int get_starve_steps() const { return starve_steps; }
+    void reset_starve_steps() { steps_until_starve = BUG_STARVE; }
 
     virtual void move(Organism*** world);
 
 private:
-    int starve_steps;
+    int steps_until_starve;
 };
 
 void Doodlebug::move(Organism*** world) {
+    // eating or moving 
     Position ori_pos = get_position();
-
     Position new_pos;
-    vector<Position> ant_positions = get_target_positions(world, WORLD_HEIGHT, WORLD_WIDTH, ANT);
+    vector<Position> ant_positions = get_target_positions(world, ANT);
     int ant_num = ant_positions.size();
     if (ant_num != 0) { // eat
+        reset_starve_steps();
         new_pos = ant_positions[rand() % ant_num];
     } else { // move
+        steps_until_starve--;
         new_pos = get_random_position(world);
     }
 
@@ -173,11 +196,20 @@ void Doodlebug::move(Organism*** world) {
         world[ori_pos.h][ori_pos.w] = new Organism;
     }
     set_moved(true);
-    // breed
-    // vector<Position> positions;
+    
+    // breeding
+    Position breed_pos = breed(world);
+    if (breed_pos.h != -1) {
+        world[breed_pos.h][breed_pos.w] = new Doodlebug(breed_pos);
+    }
+
+    // starving
+    if (steps_until_starve == 0) {
+        Position pos = get_position();
+        world[pos.h][pos.w] = new Organism;
+        delete this;
+    }
 }
-
-
 
 
 class Grid
@@ -188,17 +220,17 @@ public:
 
     void initialize(int num_ants, int num_bugs);
 
-    Organism* get_grid(int h, int w) const { return world[h][w]; }
     Organism*** get_world() const { return world; }
 
     void update_world(Organism*** new_world) { world = new_world; }
     void critters_step(char type);
-    void step_forward();
+    void step_forward(bool verbose);
 
     void reset_moved();
     
-    friend ostream &operator<<(ostream &outs, const Grid g);
     void print_prompt() const { cout << "Press ENTER to continue\n"; };
+    void print_world() const;
+    void print_statistics() const;
 
 private:
     int height, width;
@@ -206,17 +238,6 @@ private:
     Organism*** world;
     bool verbose;
 };
-
-ostream &operator<<(ostream &outs, const Grid g) { 
-    outs << "\nWorld at time step " << g.step << ":\n\n";
-    for (int h = 0; h < WORLD_HEIGHT; h++) {
-        for (int w=0; w < WORLD_WIDTH; w++) {
-            outs << *(g.get_grid(h, w)) << ' ';
-        }
-        outs << '\n';
-    }
-    return outs; 
-}
 
 Grid::Grid(int H, int W) : height(H), width(W), step(0) {
     world = new Organism**[H];
@@ -267,8 +288,7 @@ void Grid::initialize(int num_ants, int num_bugs) {
         }
     }
 
-    cout << *this << '\n';
-    print_prompt();
+    print_world();
 }
 
 void Grid::critters_step(char type) {
@@ -278,21 +298,23 @@ void Grid::critters_step(char type) {
             critter = world[h][w];
             if ((critter->get_type() == type) && (critter->has_moved() == false)) {
                 critter->move(world);
-
-                // cout << *this << '\n';
             }
         }
     }
 }
 
-void Grid::step_forward() {
+void Grid::step_forward(bool verbose) {
     step++;
     critters_step(DOODLEBUG); 
-    cout << "After bugs move" << *this << '\n';
+    if (verbose) { 
+        cout << "After bugs move";
+        print_world();
+        print_statistics();
+    }
     critters_step(ANT); 
-    cout << "After ants move" << *this << '\n';
+    if (verbose) cout << "After ants move";
+    print_world();
     reset_moved();
-    print_prompt(); 
 }
 
 void Grid::reset_moved() {
@@ -303,17 +325,44 @@ void Grid::reset_moved() {
     }
 }
 
+void Grid::print_world() const {
+    cout << "\nWorld at time step " << step << ":\n\n";
+    for (int h = 0; h < WORLD_HEIGHT; h++) {
+        for (int w=0; w < WORLD_WIDTH; w++) {
+            cout << *(world[h][w]) << ' ';
+        }
+        cout << '\n';
+    }
+    cout << "\nPress ENTER to continue\n";
+}
+
+void Grid::print_statistics() const {
+    int ants = 0, bugs = 0;
+    char type;
+    for (int h = 0; h < WORLD_HEIGHT; h++) {
+        for (int w=0; w < WORLD_WIDTH; w++) {
+            switch (world[h][w]->get_type()) {
+            case ANT:
+                ants++; break;
+            case DOODLEBUG:
+                bugs++; break;
+            }
+        }
+    }
+    cout << "Ants: " << ants << "\tBugs: " << bugs << "\n";
+}
+
 
 int main() {
     Grid grid(WORLD_HEIGHT, WORLD_WIDTH);
-    int num_ants = 3, num_bugs = 3;
+    int num_ants = 100, num_bugs = 5;
     grid.initialize(num_ants, num_bugs);
 
     char key;
     while ((key = cin.get()) && (key != 'q')) {
-        grid.step_forward();
+        grid.step_forward(false);
+        grid.print_statistics();
     }
 
     return 0;
 }
-
